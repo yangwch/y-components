fd3b343de40f21e7e5d2c2e4e44f76c21a55aa30
yangwenchao <wenchao.yang1@eeoa.com>
2022-10-21 10:57:40 +0800

    feat: divider style

--- src/Divider/index.tsx
+++ src/Divider/index.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import classNames from 'classnames';
+import React, { CSSProperties, HTMLAttributes } from 'react';
+import { settings } from '../utils/global';
 import './style/index.less';
+
+const prefix = settings.prefix;
 
 export type DividerType = 'horizontal' | 'vertical';
 
-interface DividerProps {
+interface DividerProps extends HTMLAttributes<HTMLDivElement> {
   type?: DividerType;
+  children: React.ReactNode;
 }
 function Divider(props: DividerProps) {
-  const { type } = props;
-  return <div>Divider</div>;
+  const { type, children, ...rest } = props;
+
+  const direction: string = type || 'horizontal'
+  const cls = classNames(
+    `${prefix}-divider`,
+    `${prefix}-divider-${direction}`,
+  );
+  return (
+    <div className={cls} {...rest}>
+      <span className="text">{children}Divider</span>
+    </div>
+  );
 }
 
 export default Divider;
