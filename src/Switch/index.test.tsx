3dc1b1bbc5c2e0e3b2d84aca4889d563fd7155cd
yangwenchao <wenchao.yang1@eeoa.com>
2023-02-17 17:35:27 +0800

    feat: Switch test

---
+++ src/Switch/index.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { mountTest } from '../../test/shared/mountTest';
+
+import Switch from './index';
+import { settings } from '../utils/global';
+
+describe('Switch', () => {
+  mountTest(() => <Switch />)
+  mountTest(() => <Switch disabled checked />)
+  mountTest(() => <Switch checkedText="o" uncheckedText="|" />)
+
+  it('should render checkedText corrected', () => {
+    const uncheckedText = '关';
+    const checkedText = '开'
+    const renderResult1 = render(<Switch uncheckedText={uncheckedText} />)
+    const renderResult2 = render(<Switch checked checkedText={checkedText} />)
+
+    expect(renderResult1.queryByText(uncheckedText)).toBeTruthy()
+    expect(renderResult2.queryByText(checkedText)).toBeTruthy()
+  })
+
+  it('should render disabled styles', () => {
+    const { container } = render(<Switch disabled />)
+    expect(container.querySelectorAll('.yc-switch-disabled').length).toBe(1)
+    expect(container.querySelectorAll('.yc-switch-disabled input[disabled]').length).toBe(1)
+  })
+})
