4e605d30623be717ca489a5643d84d0d2277936a
yangwenchao <wenchao.yang1@eeoa.com>
2023-03-08 12:37:51 +0800

    chore: version

--- version.js
+++ version.js
@@ -1,11 +1,21 @@
-const { execSync } = require('child_process');
+const { execSync, exec } = require('child_process');
 
 const versionCmd = process.env.VERSION || 'patch';
 
-execSync(`npm version ${versionCmd}`);
+exec(`npm version ${versionCmd}`, (err) => {
+  if (err) {
+    throw err;
+  }
 
-const package = require('./package.json');
-const newVersion = package.version;
-execSync(
-  `git add package.json && git commit -m "chore: new tag ${newVersion}" && git tag ${newVersion} && git push origin ${newVersion} && git push`,
-);
+  const package = require('./package.json');
+  const newVersion = package.version;
+  exec(
+    `git add package.json && git commit -m "chore: new tag ${newVersion}" && git tag ${newVersion} && git push origin ${newVersion} && git push`,
+    (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log('complete');
+    },
+  );
+});
