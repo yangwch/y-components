6270076343410ad85ced52c6e59d38ef62af859b
yangwenchao <wenchao.yang1@eeoa.com>
2023-04-10 20:37:16 +0800

    add script version

--- version.js
+++ version.js
@@ -9,13 +9,10 @@ exec(`npm version ${versionCmd}`, (err) => {
 
   const package = require('./package.json');
   const newVersion = package.version;
-  exec(
-    `git add package.json && git commit -m "chore: new tag ${newVersion}" && git tag ${newVersion} && git push origin ${newVersion} && git push`,
-    (err) => {
-      if (err) {
-        throw err;
-      }
-      console.log('complete');
-    },
-  );
+  exec(`git push origin ${newVersion} && git push`, (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log('complete');
+  });
 });
